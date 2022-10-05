2ccaf00e07084b76dbe2d1922164d0a6b67e5adc
MatiasDonati <matod87@hotmail.com>
2022-10-05 08:50:35 -0300

    eventos clicks nuevos botones

---
+++ otraCosa.js
@@ -0,0 +1,7 @@
+const otraCosa = () => {
+    const otraCosa = document.getElementById('otra-cosa')
+    otraCosa.addEventListener('click', ()=>{
+        console.log('OTRA COSA !!');
+    })
+}
+otraCosa()
---
+++ verProductoMasCaro.js
@@ -0,0 +1,7 @@
+const masCaro = () => {
+    const masCaro = document.getElementById('mas-caro')
+    masCaro.addEventListener('click', ()=>{
+        console.log('MAS CAROO!');
+    })
+}
+masCaro()
