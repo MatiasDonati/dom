69ee97a94834ba86d45a71c43dac0125b6fbba3d
MatiasDonati <matod87@hotmail.com>
2022-10-18 13:21:08 -0300

    armado el sweetAlert para WhatsApp hay q continuar

--- whatsApp.js
+++ whatsApp.js
@@ -2,6 +2,22 @@ const whatsApp = () => {
     const whatsApp = document.getElementById('whats-app')
     whatsApp.addEventListener('click', ()=>{
         console.log('WhatsApp!');
+        // Swal.fire({
+        //     title: '<strong><i class="fa-brands fa-whatsapp"></i></strong>',
+        //     // icon: 'info',
+        //     html:
+        //       'Para enviarnos un WhatsApp clickea en siguiente enlace: ' +
+        //       '<a href="https://walink.co/52ef48">WhatsApp!</a> ',
+        //     // showCloseButton: true,
+        //     showCancelButton: true,
+        //     // focusConfirm: false,
+        //     confirmButtonText:
+        //       '<i class="fa fa-thumbs-up"></i> WhatsApp!',
+        //     confirmButtonAriaLabel: 'Thumbs up, great!',
+        //     cancelButtonText:
+        //       '<i class="fa fa-thumbs-down"></i>',
+        //     cancelButtonAriaLabel: 'Thumbs down'
+        //   })
     })
 }
 whatsApp()
