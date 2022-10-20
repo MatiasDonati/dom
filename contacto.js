9748f3754d8bdd0db514128400f9acc129035560
MatiasDonati <matod87@hotmail.com>
2022-10-20 01:11:25 -0300

    formulario contacto

--- contacto.js
+++ contacto.js
@@ -5,25 +5,21 @@ botonContacto.addEventListener('click', ()=>{
     contenedorProductos.innerHTML= ""
     contenedorProductos.innerHTML=`
         <div class="container">
-            <form class="row g-3 mt-3">
+            <form id="form" class="row g-3 mt-3">
                 <div class="col6">
                 <label for="from_name">Tu nombre:</label>
                 <input type="text" class="form-control" id="from_name" placeholder="Escribe tu Nombre...">
                 </div>
                 <div class="col6">
                     <label for="message">Mensaje</label>
-                    <input type="text" class="form-control" name="message" id="message" placeholder="Escribe tu Mensaje...">
+                    <textarea type="text" class="form-control" name="message" id="message" placeholder="Escribe tu Mensaje..."></textarea>
                 </div>
                 <div class="col6">
                     <label for="email_id">Tu Email:</label>
                     <input type="text" class="form-control" name="email_id" id="email_id" placeholder="Escribe tu Email...">
                 </div>
                 <div class="col6">
-                    <label for="reply_to">reply_to</label>
-                    <input type="text" class="form-control" name="reply_to" id="reply_to">
-                </div>
-                <div class="col6">
-                    <button type="submit" class="btn btn-primary"> 
+                    <button type="submit" id="button" class="btn btn-primary" value="Send Email"> 
                     Enviar
                     </button>
                 </div>
@@ -32,3 +28,4 @@ botonContacto.addEventListener('click', ()=>{
     `
     }
 )
+
